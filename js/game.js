5403522a9664105b0d36c689bd94c8d5a9054420
Shivam Sharma <shivam231198@gmail.com>
2020-01-08 03:38:49 +0530

    build basic game functionality

---
+++ js/game.js
@@ -0,0 +1,91 @@
+var score = 0;
+
+function gameStart() {
+
+    //User Score
+    score = 0;  //points
+
+    // Reset
+    $('.play .row .block').removeClass('wrong');
+    $('.play .row .block').removeClass('correct');
+
+    // Game starting params
+    var id = Math.floor(Math.random() * 32) + 1;
+    $(`.play .row #${id}`).addClass('correct');
+    let timeoutBlockID = id;
+    let timeoutID = setTimeout(timeout, 5000);   //Initial timeout is set to 5 seconds
+
+
+    // varify click
+    function compare(blockID) {
+        if(blockID === id) {
+            // +1 points for matched block            
+            score++;
+
+            // setting new block
+            $('.play .row .block').removeClass('correct');
+            id = Math.floor(Math.random() * 32) + 1;
+            $(`.play .row #${id}`).addClass('correct');
+
+            timeoutBlockID = id;
+
+            // setting Timeout according to score
+            if(score == 1) {
+                timeoutID = setTimeout(timeout, 4000);   //timelimit = 4sec when score = 1 
+            } else if(score == 2) {
+                timeoutID = setTimeout(timeout, 3000);   //timelimit = 3sec when score = 2
+            } else if(score == 3) {
+                timeoutID = setTimeout(timeout, 2000);   //timelimit = 2sec when score = 3
+            } else if(score > 3 && score <= 8) {
+                timeoutID = setTimeout(timeout, 1000);   //timelimit = 1sec when score = 4, 5, 6, 7, 8
+            } else if(score > 8 && score <= 10) {
+                timeoutID = setTimeout(timeout, 800);   //timelimit = 0.8sec when score = 9, 10
+            } else if(score > 10 && score <= 12) {
+                timeoutID = setTimeout(timeout, 700);   //timelimit = 0.7sec when score = 11, 12
+            } else if(score > 12 && score <= 14) {
+                timeoutID = setTimeout(timeout, 600);   //timelimit = 0.6sec when score = 13, 14
+            } else if(score > 14) {
+                timeoutID = setTimeout(timeout, 500);   //timelimit = 0.5sec when score > 14 (15,16...)
+            }
+        } else {
+            $('.result').addClass('show');
+
+            $(`.play .row #${blockID}`).addClass('wrong');
+
+            console.log('NOT Matched !  GAME OVER...');
+            console.log(`Player Score: ${score}`);
+        }
+    }
+
+    //End Game when Timeout
+    function timeout() {
+        $('.result').addClass('show');
+
+        $('.play .row .block').removeClass('correct');
+        $(`.play .row #${timeoutBlockID}`).addClass('wrong');
+        
+        console.log('TIME OUT !  GAME OVER...');
+        console.log(`Player Score: ${score}`);
+    }
+
+    // Handle click events
+    $('.play .row .block').click(function(){
+        clearTimeout(timeoutID);
+        let clickedID = $(this).attr('id');
+        compare(parseInt(clickedID, 10));
+    });
+}
+
+
+// When clicked on the start button...
+$('.home .start').click(function(){
+    $('.home').addClass('hide');
+    $('.play').addClass('show');
+    $('.play .row .block').removeClass('correct');
+    $('.play .row .block').removeClass('wrong');
+    gameStart();
+});
+
+
+   
+ 
