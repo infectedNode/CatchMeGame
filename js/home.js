e606a72b3fd5c63bd0c252511f8774b3c67df41e
Shivam Sharma <shivam231198@gmail.com>
2020-01-09 04:18:35 +0530

    github url

--- js/home.js
+++ js/home.js
@@ -1,6 +1,7 @@
 
 // When clicked on the start button...
 $('.home .start').click(function(){
+    // let url = "CatchMeGame/game.html";  // for github purpose
     let url = "game.html";
     window.location.href = url; 
 });
