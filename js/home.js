1c26642a0efe4938b649ab22a29ab13eb2b11f36
Shivam Sharma <shivam231198@gmail.com>
2020-01-09 04:21:47 +0530

    github url error resolved

--- js/home.js
+++ js/home.js
@@ -1,7 +1,6 @@
 
 // When clicked on the start button...
 $('.home .start').click(function(){
-    let url = "CatchMeGame/game.html";  // for GitHub purpose
-    // let url = "game.html";
+    let url = "game.html";
     window.location.href = url; 
 });
